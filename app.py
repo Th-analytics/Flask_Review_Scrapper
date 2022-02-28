35c25b9b4b4864688e76430326072fe28fb2993f
Aditya Mishra <adityamishra2198@gmail.com>
2022-03-01 00:17:43 +0530

    m

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from flask import Flask,render_template, request, redirect, url_for
 import requests
 from bs4 import BeautifulSoup as BS
 from urllib.request import urlopen as uReq
-
+import threading
 import pymongo
 
 
@@ -15,11 +15,28 @@ from flask_cors import cross_origin
 
 from reviewScrapper import Scrap
 app = Flask(__name__)
+class threadClass:
 
+    def __init__(self,):
+
+        thread = threading.Thread(target=self.run, args=())
+        thread.daemon = True  # Daemonize thread
+        thread.start()  # Start the execution
+
+    def run(self):
+        global free_status
+        free_status = False
+        free_status = True
 @app.route('/', methods=['POST', 'GET'])
 def homepage():
     obj = Scrap()
     if request.method == 'POST':
+        global free_status
+        ## To maintain the internal server issue on heroku
+        if free_status != True:
+            return "This website is executing some process. Kindly try after some time..."
+        else:
+            free_status = True
         searched_product_name = request.form['content'].replace(' ', '')
         number_of_product = int(request.form['expected_review'])
         obj.product = searched_product_name
